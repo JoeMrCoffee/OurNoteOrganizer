adb5aaab16af9e3e31883bcacd3ee47d43bc778b
Joe <45982832+JoeMrCoffee@users.noreply.github.com>
2022-10-12 18:19:33 +0800

    Update noteshome.php
    Include the Unique ID of the posts - better in case the user enters 2 posts of the same name. Also helps with the apostrophe in the name issue.

--- work/src/noteshome.php
+++ work/src/noteshome.php
@@ -52,6 +52,7 @@
             $postname=$note['postname'];
             $postcolor=$note['postcolor'];
             $postdate = $note['date'];
+	    $postid = $note['_id'];
             $postimage = "";
             if(isset($note['postimage'])){ $postimage = $note['postimage']; }
             
@@ -78,6 +79,7 @@
                     <input type='hidden' name='name' value='$name'>
                     <input type='hidden' name='postname' value='$postname'>
                     <input type='hidden' name='postcolor' value='$postcolor'>
+		    <input type='hidden' name='postid' value='$postid'>
                     <input type='submit' value='VIEW' class='view'></div></form></td>";
             
                 $item++;
@@ -95,6 +97,7 @@
                     <input type='hidden' name='name' value='$name'>
                     <input type='hidden' name='postname' value='$postname'>
                     <input type='hidden' name='postcolor' value='$postcolor'>
+		    <input type='hidden' name='postid' value='$postid'>
                     <input type='submit' value='VIEW' class='view'></div></form></td>";
             
             }
